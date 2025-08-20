3c1937eeac7af79285e6a85c6adc929815807eca
bilal00999 <bilalahmed20051@gmail.com>
2025-08-20 17:51:40 +0500

    add project 2

---
+++ 07contextapi2/src/App.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { ThemeProvider } from "./context/ThemeContext";
+import Btn from "./components/Btn";
+import Card from "./components/Card";
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+      <Btn />
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4"></div>
+          <div className="flex flex-wrap gap-y-3">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
